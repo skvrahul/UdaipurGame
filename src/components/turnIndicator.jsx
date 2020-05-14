cf0773e940deb1db86f8f3ba920d75fd9cf68ab8
Rahul Sathanapalli <skvrahul@gmail.com>
2020-05-14 23:52:02 +0530

    UI Changes - (Add flash in turnIndicator)

--- src/components/turnIndicator.jsx
+++ src/components/turnIndicator.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import "./styles/turnIndicator.css";
 class TurnIndicator extends Component {
   render() {
     const playerID = this.props.playerID;
     const currentPlayer = this.props.currentPlayer;
     const iAmActive = currentPlayer === playerID;
-    return <div>{iAmActive ? "Your turn" : "Opponents turn"}</div>;
+    return (
+      <div
+        className={"turn-indicator " + (iAmActive ? "active" : null)}
+        style={{ fontSize: "20px" }}
+      >
+        {iAmActive ? "Your turn" : "Opponents turn"}
+      </div>
+    );
   }
 }
 
