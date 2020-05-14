9a971c66d586ee29d6cf5912266faeb2182ba3ad
Rahul Sathanapalli <skvrahul@gmail.com>
2020-05-15 01:38:14 +0530

    Set production to true

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ export const GAME_NAME = "udaipur";
 export const GAME_SERVER_PORT = 8000;
 export const GAME_SERVER_URL = `http://localhost:${GAME_SERVER_PORT}`;
 export const WEB_SERVER_URL = "http://localhost:8000";
-export const APP_PRODUCTION = false;
+export const APP_PRODUCTION = true;
