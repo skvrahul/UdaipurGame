a76e51770768b77b6239e5c13e2de72012743054
Rahul Sathanapalli <skvrahul@gmail.com>
2020-05-15 13:16:16 +0530

    Removes unused imports and minor CSS changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 // src/App.js
-import { UdaipurGame } from "./game/game.js";
-import { Client } from "boardgame.io/react";
-import UdaipurBoard from "./components/board.jsx";
-import { SocketIO } from "boardgame.io/multiplayer";
 import React from "react";
 import { Switch, Route, useHistory } from "react-router";
 import HomePage from "./components/homepage.jsx";
-import { GAME_SERVER_URL } from "./config.js";
 import Lobby from "./components/lobby.jsx";
 
 function App() {
