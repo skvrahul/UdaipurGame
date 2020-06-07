ac2cf6f3df106f493c1b19e09381315a237f20d2
Rahul Sathanapalli <skvrahul@gmail.com>
2020-06-07 14:41:49 +0530

    Adds audible alert for when its your turn

--- src/components/turnIndicator.jsx
+++ src/components/turnIndicator.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import "./styles/turnIndicator.css";
+const audio = new Audio(
+  "https://freesound.org/data/previews/320/320654_5260872-lq.mp3"
+);
 class TurnIndicator extends Component {
+  state = {
+    played: false,
+  };
   render() {
     const playerID = this.props.playerID;
     const currentPlayer = this.props.currentPlayer;
     const iAmActive = currentPlayer === playerID;
+    if (iAmActive && !this.state.played) {
+      audio.play();
+      this.setState({ played: true });
+    }
+    if (!iAmActive && this.state.played) {
+      this.setState({ played: false });
+    }
     return (
       <div
         className={"turn-indicator " + (iAmActive ? "active" : null)}
